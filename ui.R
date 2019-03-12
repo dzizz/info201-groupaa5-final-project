b6224d65db97d42bb3a43c721f3de0d22e66c547
Arasant <arasant@uw.edu>
2019-03-12 16:04:45 -0700

    Create new UI layout

--- ui.R
+++ ui.R
@@ -5,17 +5,68 @@ source("server.R")
 #Create the main user interface using a fluidPage
 main_ui <- fluidPage(
   titlePanel("Title"),
-  sidebarLayout(
-    sidebarPanel(
-    ),
-    mainPanel(
-      tabsetPanel(type = "tabs",
-                  tabPanel("Table"),
-                  tabPanel("1"),
-                  tabPanel("2"),
-                  tabPanel("3"),
-                  tabPanel("4")
-      )
-    )
+  tabsetPanel(type = "tabs",
+              tabPanel("Home",
+                       sidebarLayout(
+                         sidebarPanel(
+                           
+                         ),
+                         mainPanel(
+                           tabsetPanel(type = "tabs",
+                                       tabPanel("Summary"),
+                                       tabPanel("Data")
+                           )
+                         )
+                       )),
+              tabPanel("1",
+                       sidebarLayout(
+                         sidebarPanel(
+                           
+                         ),
+                         mainPanel(
+                           tabsetPanel(type = "tabs",
+                                       tabPanel("Summary"),
+                                       tabPanel("Plot")
+                           )
+                         )
+                       )),
+              tabPanel("2",
+                       sidebarLayout(
+                         sidebarPanel(
+                           
+                         ),
+                         mainPanel(
+                           tabsetPanel(type = "tabs",
+                                       tabPanel("Summary"),
+                                       tabPanel("Plot")
+                           )
+                         )
+                       )),
+              tabPanel("3",
+                       sidebarLayout(
+                         sidebarPanel(
+                           
+                         ),
+                         mainPanel(
+                           tabsetPanel(type = "tabs",
+                                       tabPanel("Summary"),
+                                       tabPanel("Plot",
+                                                 plotOutput("rico_plot"))
+                           )
+                         )
+                       )),
+              tabPanel("4",
+                       sidebarLayout(
+                         sidebarPanel(
+                           
+                         ),
+                         mainPanel(
+                           tabsetPanel(type = "tabs",
+                                       tabPanel("Summary"),
+                                       tabPanel("Plot")
+                           )
+                         )
+                       ))
   )
 )
+
