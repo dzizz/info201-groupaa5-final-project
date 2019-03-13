76f8b8ceed83758a1bb0e22c66ba116d601672b2
Kayla Lee <kayla.lee44@outlook.com>
2019-03-13 00:33:25 -0700

    Description

--- ui.R
+++ ui.R
@@ -10,10 +10,38 @@ main_ui <- fluidPage(
                   tabPanel("Introduction", 
                     tags$h3("Country Level HDI and CO2 Emission Data Analysis"),
                     tags$div(
-                      tags$p("This is an analysis of country-level data Human Development Index (HDI) and carbon dioxide emissions per capita provided by the United Nations Development Program's Human Development Reports."),
-                      tags$p(paste0("The Human Development Index is a numerical ranking of countries on the basis of overall quality of life produced by the United Nations. It operates on a scale from 0 to 1, with 0 being the worst and 1 being the best. As detailed on the "), tags$a(href="http://hdr.undp.org/en/content/human-development-index-hdi", "UN Website"), ", this evaluation is done on a broad basis of the three dimensions: \"long and healthy life\", \"knowledge\", and \"a decent standard of living\", comprised of life expectancy, average years of schooling, and gross national income per capita respectively. Gross national income per capita is itself a complex measure, but in effect indicates the overall wealth generated in that country, divided by the number of people in the country to get the amount of wealth available per person."),
-                      tags$p("Carbon dioxide emissions per capita is more straightforward, representing the total carbon dioxide emitted by that country divded by the total population of the country. This is a valuable statistic because it shows which countries are polluting more on the whole while removing the overwhelming statistical weight that countries with very large populations like China, India, and the United States add."),
-                      tags$p("This data is valuable to analyze together because it shows some of the less explored elements of the developed versus developing world. If a country like Norway, with the highest HDI in the world, shows a corresponding increase or decrease in carbon dioxide emissions per capita, it represents a meaningful environmental impact of increading human development. If Norway is good, then it gives all the more reason to encourage poorer countries to increase their quality of life. If Norway is bad, it may give pause to the pursuit of modernization, perhaps prompting questions reevaluating what we value as a society and how we go about achieving a high quality of life.")
+                      tags$p("This is an analysis of country-level data Human Development Index 
+                             (HDI) and carbon dioxide emissions per capita provided by the United 
+                             Nations Development Program's Human Development Reports."),
+                      tags$p(paste0("The Human Development Index is a numerical ranking of 
+                                    countries on the basis of overall quality of life produced by 
+                                    the United Nations. It operates on a scale from 0 to 1, with 0 
+                                    being the worst and 1 being the best. As detailed on the "), 
+                             tags$a(href="http://hdr.undp.org/en/content/human-development-
+                                    index-hdi", "UN Website"), ", this evaluation is done on a 
+                             broad basis of the three dimensions: \"long and healthy life\", 
+                             \"knowledge\", and \"a decent standard of living\", comprised of 
+                             life expectancy, average years of schooling, and gross national 
+                             income per capita respectively. Gross national income per capita is 
+                             itself a complex measure, but in effect indicates the overall wealth 
+                             generated in that country, divided by the number of people in the 
+                             country to get the amount of wealth available per person."),
+                      tags$p("Carbon dioxide emissions per capita is more straightforward, 
+                             representing the total carbon dioxide emitted by that country divded 
+                             by the total population of the country. This is a valuable statistic 
+                             because it shows which countries are polluting more on the whole while 
+                             removing the overwhelming statistical weight that countries with very 
+                             large populations like China, India, and the United States add."),
+                      tags$p("This data is valuable to analyze together because it shows some of the 
+                             less explored elements of the developed versus developing world. If a 
+                             country like Norway, with the highest HDI in the world, shows a 
+                             corresponding increase or decrease in carbon dioxide emissions per 
+                             capita, it represents a meaningful environmental impact of increading 
+                             human development. If Norway is good, then it gives all the more reason 
+                             to encourage poorer countries to increase their quality of life. If 
+                             Norway is bad, it may give pause to the pursuit of modernization, 
+                             perhaps prompting questions reevaluating what we value as a society and 
+                             how we go about achieving a high quality of life.")
                   )),
                     tabPanel("HDI Data", dataTableOutput("HDI_data")),
                     tabPanel("CO2 Data", dataTableOutput("CO2_data")), 
@@ -126,7 +154,7 @@ emissions? Will this pave a way for other nations modernizing to follow in their
                            
                            selectInput(
                              inputId = "CO2_countries",
-                             label = "Select Country (CO2):",
+                             label = "Select Country (CO2 Levels):",
                              choices = c("Select country...", as.character(co2_data$Country)),
                              selected = "Select country..."
                            )
@@ -137,35 +165,47 @@ emissions? Will this pave a way for other nations modernizing to follow in their
                                                 tags$div(
                                                   tags$h4(
                                                     tags$strong("My Question:")),
-                                                  tags$p("Which two countries are 
-most similar in terms of HDI and CO2 emissions? Which two are most different? Why might this be - 
+                                                  tags$p("What countries are 
+most similar in terms of HDI and CO2 emissions? Which are the most different? Why might this be - 
 that is, what factors may influence this?"),
                                                   tags$h4(
                                                     tags$strong("Relavence/Interest:")),
                                                   tags$p("This is a question of interest because we 
 can compare and contrast countries and determine why they might be different or similar. 
 By arranging the data from highest HDI or highest CO2 or by lowest HDI or lowest CO2 on a map, 
-we can view which countries are most alike or unalike. WHO WOULD WANT THIS INFORMATION, WHY, AND
-                                                         FOR WHAT"),
+we can view which countries are most alike or unalike. Then, we can wonder what sorts of factors 
+cause certain countries to be similar or different. Some factors may include, wealth, population,
+number of cities, industries, laws, culture, etc."),
+                                                  tags$p("Scientists could potentially want this
+information to determine what factors cause high CO2 emission levels in certain countries and 
+what factors cause this. They would be able to see the similarities between certain countries and 
+determine specific correlations, such as high/low HDI values. This would allow scientists dive 
+deeper into this relationships and find even more specific factors, for example, high HDI is an
+indication of happiness levels among the population, which can mean there are large industries and 
+cities within the country. This could potentially mean that due to the large amount of human activity
+and machinery, the CO2 emissions are very high. By determining information like this, 
+scientists would be able to tell the public what sorts of factors are causing/aiding in 
+high CO2 emission values and what we can do to reduce it."),
                                                   tags$h4(
                                                     tags$strong("Quantitative Analysis:")),
-                                                  tags$p("By ordering the joined data with the HDI 
-and CO2 emission values from 2014, we can see which countries are the most similar in terms of HDI 
-and CO2 emission values. Looking at the data ordered by CO2 emissions from largest to smallest, we 
-can see that the top two countries in terms of largest CO2 emissions are Qatar and Trinidad and 
-Tobago. Additionally, the most similar countries in terms of largest HDI values are Norway and 
-Switzerland. On the flip side, when looking that countries that have the lowest CO2 emission and 
-HDI values, we can see that there are multiple countries that have the same values, in addition to 
-many other countries having the same values throughout the data."),
-                                                  tags$p("Also, by having ordered data, we can 
-further compare data by seeing the highest and lowest values for HDI and CO2 emission values. The 
-two countries that have the largest difference between them in terms of CO2 emission values are 
-Qatar and Burundi. The two countries that have the largest difference between them in terms of HDI 
-                                                         values are Norway and Marshall Islands."),
+                                                  tags$p("By comparing the HDI 
+and CO2 emission values in each country, we can see which countries are the most similar in terms of HDI 
+and CO2 emission values. Looking at the map data, we can see that there does appear to 
+be a slight correlation in HDI and CO2 levels. For example, looking at Adorra's CO2 and HDI levels 
+in 2014 for example, the HDI and CO2 values are both quite high, as seen by the hue on the map. Also,
+by selecting the country and year specifically, we can see the values. This could potentially suggest that
+the happier people are the more CO2 emissions are produced. On the other hand, looking at 
+Afghanistan's CO2 and HDI levels in 2014, we see that the values are very low. We could speculate
+that this might be due to the War in Afghanistan, which caused many people to lose their homes and
+loved ones - which is why the HDI value is so low. In relation to CO2 emissions, the HDI 
+levels could also suggest that industries slowed down or there is less civilization."),
+                                                  tags$p("From the map alone, we cannot see any
+outstanding outliers. Outliers are more clearly seen through scatter plots or bar graphs, 
+as seen on the other visualizations within this report."),
                                                   tags$p("Based on this data, we can further look 
-into what other factors may influence two specific countries and why their HDI/CO2 emissions are 
+into what other factors may influence specific countries and why their HDI/CO2 emissions are 
                                                          similar/different."))),
-                                       tabPanel("Plot",
+                                       tabPanel("Maps",
                                                 plotOutput("kaylaCO2Map"), plotOutput("kaylaHDIMap")
                                        )
                            )
